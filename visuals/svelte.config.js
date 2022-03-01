defe47a1f2e3589d2cc7dd232639fa3b617ccf5f
Jason Kao <jason.kao@columbia.edu>
2022-03-01 15:13:04 -0500

    some purging, some reworking

--- visuals/svelte.config.js
+++ visuals/svelte.config.js
@@ -2,7 +2,6 @@ import static_adapter from '@sveltejs/adapter-static';
 
 const config = {
 	kit: {
-		target: '#svelte',
 		adapter: static_adapter(),
 		paths: {
 			base: process.env.NODE_ENV === 'development' ? '' : '/asam-redistricting-maps',
